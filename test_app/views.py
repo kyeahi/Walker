962f194bd17d0e9c992505986fd5f45254030b6b
manja1990 <manja1990@naver.com>
2022-05-03 15:34:17 +0900

    git-django-mysql 연동 test

---
+++ test_app/views.py
@@ -0,0 +1,10 @@
+from django.shortcuts import render
+
+# Create your views here.
+from django.http import HttpResponse
+from django.shortcuts import render
+
+# Create your views here.
+def func1(request):
+
+    return HttpResponse("Hello I'm Noob")
