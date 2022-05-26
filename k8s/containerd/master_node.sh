1503b4b56befd38443af10078a56b22be226189a
jenkins_token <97927143+Gimbitgaram@users.noreply.github.com>
2022-05-26 16:43:20 +0900

    Update master_node.sh

--- k8s/containerd/master_node.sh
+++ k8s/containerd/master_node.sh
@@ -1,5 +1,7 @@
+## CNI로 calico 를 사용
+
 kubeadm init --token 123456.1234567890123456 --token-ttl 0 \
---pod-network-cidr=172.16.0.0/16 --apiserver-advertise-address=192.168.1.10 
+--apiserver-cert-extra-sans=192.168.56.100 \ --control-plane-endpoint=192.168.56.100:6443
 
 # config for master node only 
 mkdir -p $HOME/.kube
@@ -7,5 +9,5 @@ cp -i /etc/kubernetes/admin.conf $HOME/.kube/config
 chown $(id -u):$(id -g) $HOME/.kube/config
 
 # config for kubernetes's network 
-kubectl apply -f \
-https://raw.githubusercontent.com/sysnet4admin/IaC/master/manifests/172.16_net_calico.yaml
+curl https://docs.projectcalico.org/manifests/calico.yaml -O
+kubectl apply -f calico.yaml
