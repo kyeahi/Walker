205e5b9d7145135f7f6ddf880fa30a690aecb0e0
AnHyunDong <81276472+SpamDong@users.noreply.github.com>
2022-06-07 13:42:49 +0900

    db change

--- config/settings.py
+++ config/settings.py
@@ -77,8 +77,15 @@ WSGI_APPLICATION = 'config.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'walkingproject',
+        'USER': 'root',
+        'PASSWORD': 'qwer1234',
+        'HOST': 'localhost',
+        'PORT': '3306',
+        'OPTIONS': {
+            'init_command': 'SET sql_mode="STRICT_TRANS_TABLES"'
+        }
     }
 }
 
